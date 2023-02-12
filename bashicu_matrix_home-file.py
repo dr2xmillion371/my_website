cb1e050c62ba5e61b6d650d1e210fc9428cc4fea
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-02-12 13:42:06 -0600

    home-file

---
+++ bashicu_matrix_home-file.py
@@ -0,0 +1,21 @@
+import time as tea
+
+def bm_home():
+	print('This is lngi for BMS!')
+	print('Here is goes!:')
+	print('(0)')
+	print('(0)(0)')
+	print('(0)(0)(0)')
+	print('(0)(0)(0)(0)')
+	print('(0)(0)(0)(0)(0)')
+	print('(0)(0)(0)(0)(0)(0)')
+	print('(0)(0)(0)(0)(0)(0)(0)')
+	print('(0)(1)')
+	print('(0)(1)(0)(1)')
+	print('(0)(1)(0)(1)(0)(1)')
+	print('(0)(1)(0)(1)(0)(1)(0)(1)')
+	print('...')
+
+
+
+bm_home()
